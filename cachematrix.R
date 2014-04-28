dc8b1b5fd2666c1346a2fb00d194da217cf2c6be
ElFuturero <hector.e.meza@gmail.com>
2014-04-27 17:35:54 -0700

    Increased indentation to improve readability.

--- cachematrix.R
+++ cachematrix.R
@@ -15,18 +15,18 @@
 ## d) gets the value of the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-  i <- NULL
-  set <- function(y) {
-    x <<- y
-    i <<- NULL
-  }
-  set(x)
-  get <- function() x
-  setinv <- function(inv) i <<- inv
-  getinv <- function() i
-  list(set = set, get = get,
-       setinv = setinv,
-       getinv = getinv)
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    set(x)
+    get <- function() x
+    setinv <- function(inv) i <<- inv
+    getinv <- function() i
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
@@ -37,13 +37,13 @@ makeCacheMatrix <- function(x = matrix()) {
 ## computes and returns the value.
 
 cacheSolve <- function(x, ...) {
-  i <- x$getinv()
-  if(!is.null(i)) {
-    message("getting cached data")
-    return(i)
-  }
-  data <- x$get()
-  i <- solve(data, ...)
-  x$setinv(i)
-  i
+    i <- x$getinv()
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setinv(i)
+    i
 }
