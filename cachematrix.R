a06468d7d7a1d114a452e5407191b7490834fdf2
ElFuturero <hector.e.meza@gmail.com>
2014-04-27 17:28:52 -0700

    Both functions, "makeCacheMatrix" and "cacheSolve", have been written.
    Explanatory comments written about what the functions do.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions "makeCacheMatrix" and "cacheSolve", cache the
+## inverse of a given matrix, since matrix inversion can be a costly
+## computation.
+##
+## Assuption: the following two functions assume that the matrix that we
+##            are given is a square invertible matrix.
 
-## Write a short comment describing this function
+
+
+## "MakeCacheMatrix" returns a special object, which is in reality a list
+## with a function that:
+## a) sets the value of a given matrix
+## b) gets the value of the matrix
+## c) sets the value of the inverse of the matrix
+## d) gets the value of the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  set(x)
+  get <- function() x
+  setinv <- function(inv) i <<- inv
+  getinv <- function() i
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## "cacheSolve" first checks to see if the inverse of the special matrix
+## created by "makeCacheMatrix" has been already been computed. If the 
+## inverse has already been calculated, it returns the value of "i"
+## that has been stored in the global environment -- otherwise, it
+## computes and returns the value.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinv()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinv(i)
+  i
 }
